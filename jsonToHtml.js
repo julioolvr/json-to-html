a3b78bb03efb67deea09ec3fa93f3d78ef9de0e9
Julio Olivera <julio.olvr@gmail.com>
2012-09-25 21:59:33 -0300

    Basic idea

---
+++ jsonToHtml.js
@@ -0,0 +1,43 @@
+function jsonToHtml(json) {
+  function buildStartTag(element, attributes) {
+    var tag = "<" + element;
+    var values = [];
+
+    for (var att in attributes)
+      values.push(att + "=\"" + attributes[att] + "\"");
+
+    if (values.length)
+      tag += " " + values.join(", ");
+
+    tag += ">";
+    return tag;
+  }
+
+  if (typeof json === "string")
+    json = JSON.parse(json);
+
+  var result = "";
+
+  if (json instanceof Array) {
+    result = "";
+
+    for (var i = 0; i < json.length; i++)
+      result += jsonToHtml(json[i]);
+
+    return result;
+  }
+
+  if (!json.content)
+    return "";
+
+  if (json.type === "text") {
+    result += json.content;
+  } else {
+    var startTag = buildStartTag(json.type, json.attributes);
+    var endTag = "</" + json.type + ">";
+
+    result += startTag + jsonToHtml(json.content) + endTag;
+  }
+
+  return result;
+}
---
+++ tests/tests.js
@@ -0,0 +1,69 @@
+test("Empty object", function() {
+  var json = {};
+  
+  equal(jsonToHtml(json), "", "An empty object produces an empty string.");
+});
+
+test("Empty array", function() {
+  var json = [];
+  
+  equal(jsonToHtml(json), "", "An empty array produces an empty string.");
+});
+
+test("Single text element", function() {
+  var json = {
+    type: "text",
+    content: "test text"
+  };
+
+  equal(jsonToHtml(json), "test text", "An object with only a test node returns the same text.");
+});
+
+test("Single non-text element", function() {
+  var json = {
+    type: "div",
+    content: {}
+  };
+
+  equal(jsonToHtml(json), "<div></div>", "An object with only a non-text node returns the open and close tag for that element.");
+});
+
+test("Sibling non-text elements", function() {
+  var json = [
+    {
+      type: "div",
+      content: {}
+    },
+    {
+      type: "div",
+      content: {}
+    }
+  ];
+
+  equal(jsonToHtml(json), "<div></div><div></div>", "An object with two siblings node should return one next to the other.");
+});
+
+test("Nested elements", function() {
+  var json = {
+    type: "div",
+    content: {
+      type: "div",
+      content: {}
+    }
+  };
+
+  equal(jsonToHtml(json), "<div><div></div></div>", "Nested elements in the JSON object are nested in the HTML.");
+});
+
+test("Element's attributes", function() {
+  var json = {
+    type: "div",
+    attributes: {
+      "class": "test-class",
+      "id": "test-id"
+    },
+    content: {}
+  };
+
+  equal(jsonToHtml(json), '<div class="test-class", id="test-id"></div>', "Elements get the attributes specified in the JSON.");
+});
